a28183834a72a7dd618ae77d9518f4ae820a03cc
JWorth06 <JWorth06@users.noreply.github.com>
2017-04-18 23:52:54 -0500

    Core Files needed to run Pingster
    Still need additional files for google authentication to run it

---
+++ bot.js
@@ -0,0 +1,183 @@
+'use strict';
+
+const RtmClient = require('@slack/client').RtmClient;
+const MemoryDataStore = require('@slack/client').MemoryDataStore;
+const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+var WebClient = require('@slack/client').WebClient;
+var token = process.env.SLACK_APP_TOKEN;
+var web = new WebClient(token);
+const request = require('request');
+
+class Bot {
+  constructor(opts) {
+    let slackToken = opts.token;
+    let autoReconnect = opts.autoReconnect || true;
+    let autoMark = opts.autoMark || true;
+
+    this.slack = new RtmClient(slackToken, { 
+      // Sets the level of logging we require
+      logLevel: 'error', 
+      // Initialize a data store for our client, 
+      // this will load additional helper
+      // functions for the storing and retrieval of data
+      dataStore: new MemoryDataStore(),
+      // Boolean indicating whether Slack should automatically 
+      // reconnect after an error response
+      autoReconnect: autoReconnect,
+      // Boolean indicating whether each message should be marked
+      // as read or not after it is processed
+      autoMark: autoMark
+    });
+	
+	// The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload if you want to cache it
+	/*
+	this.slack.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
+	  console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+	  console.log(JSON.stringify(rtmStartData));
+	  web = new WebClient(rtmStartData.self.access_token);
+	});
+	*/
+	
+    this.slack.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () => {
+      let user = this.slack.dataStore.getUserById(this.slack.activeUserId)
+      let team = this.slack.dataStore.getTeamById(this.slack.activeTeamId);
+
+      this.name = user.name;
+
+      console.log(`Connected to ${team.name} as ${user.name}`);      
+    });
+    // Create an ES6 Map to store our regular expressions
+	this.keywords = new Map();
+
+	this.slack.on(RTM_EVENTS.MESSAGE, (message) => {
+	  // Only process text messages
+	  if (!message.text) {
+		return;
+	  }
+	
+	  let channel = this.slack.dataStore.getChannelGroupOrDMById(message.channel);
+	  let user = this.slack.dataStore.getUserById(message.user);
+
+	  // Loop over the keys of the keywords Map object and test each
+	  // regular expression against the message's text property
+	  for (let regex of this.keywords.keys()) {    
+		if (regex.test(message.text)) {
+		  let callback = this.keywords.get(regex);
+		  callback(message, channel, user);
+		}
+	  }
+	
+	});
+    this.slack.start();
+  }
+  
+  respondTo(keywords, callback, start) {
+	  // If 'start' is truthy, prepend the '^' anchor to instruct the
+	  // expression to look for matches at the beginning of the string
+	  if (start) {
+		keywords = '^' + keywords;
+	  }
+
+	  // Create a new regular expression, setting the case 
+	  // insensitive (i) flag
+	  let regex = new RegExp(keywords, 'i');
+
+	  // Set the regular expression to be the key, with the callback
+	  // function as the value
+	  this.keywords.set(regex, callback);
+  }
+
+  // Send a message to a channel, with an optional callback
+  send(message, channel, cb) {
+    this.slack.sendMessage(message, channel.id, () => {
+      if (cb) {
+        cb();
+      }
+    });
+  }
+  
+  getMembersByChannel(channel) {
+    // If the channel has no members then that means we're in a DM
+    if (!channel.members) {
+      return false;
+    }
+
+    // Only select members which are not a bot
+    let members = channel.members.filter((member) => {
+      let m = this.slack.dataStore.getUserById(member);
+      // Make sure user isn't a bot
+      return (!m.is_bot);
+    });
+
+    // Get the names of the members
+    members = members.map((member) => {
+      return this.slack.dataStore.getUserById(member).name;
+    });
+
+    return members;
+  }
+  
+  //Sends a customized message to the chat using a webhook
+  sendChatMessage(Channel, textMsg){
+	web.chat.postMessage(Channel, textMsg, function(err, res) {
+		if (err) {
+			console.log('Error:', err);
+		} else {
+			console.log('Message sent: ', res);
+		}
+	});
+  }
+  
+  //Sends a customized message that can include attachments to the chat using a webhook
+  sendChatMessage(Channel, textMsg, Opts){
+	web.chat.postMessage(Channel, textMsg, Opts, function(err, res) {
+		if (err) {
+			console.log('Error:', err);
+		} else {
+			console.log('Message sent: ', res);
+		}
+	});
+  }
+  
+  //pings slack to receive a pong
+  ping(){
+	  this.slack.send({type: 'ping'}, function(err, res) {
+		if (err) {
+			console.log('Error:', err);
+		} else {
+			console.log('Message sent: ', res);
+		}
+	});
+  }
+  
+  setTypingIndicator(channel) {
+    this.slack.send({ type: 'typing', channel: channel.id });
+  }
+
+  getMemberbyName(name) {
+    return this.slack.dataStore.getUserByName(name);
+  }
+  
+  getMemberbyID(id) {
+    return this.slack.dataStore.getUserById(id);
+  } 
+  
+  getMemberDMbyID(id) {
+	  console.log(JSON.stringify(id));
+    return this.slack.dataStore.getDMById(id);
+  }
+  
+  getMemberDMbyName(name) {
+    return this.slack.dataStore.getDMByName(name);
+  }
+  
+   getChannel(id) {
+    return this.slack.dataStore.getChannelGroupOrDMById(id);
+  }
+}
+  
+  
+// Export the Bot class, which will be imported when 'require' is 
+// used
+module.exports = Bot;
---
+++ index.js
@@ -0,0 +1,562 @@
+'use strict';
+
+//building global variables
+var redis = require('redis');
+var Bot = require('./Bot');
+var fs = require('fs');
+var readline = require('readline');
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+var client = redis.createClient();
+var nodemailer = require('nodemailer');
+var twilio = require('twilio');
+var twilioclient = new twilio.RestClient(process.env.TWILIO_ACC_SID ,process.env.TWILIO_AUTH_TOKEN);
+var messages_in_channels = {};
+var pinged_in_channels = {};
+var stalked_people = {};
+var express = require('express')
+var app = express()
+var request = require('request');
+
+//Makes instance of the bot
+const bot = new Bot({
+  token: process.env.SLACKTOKEN,
+  autoReconnect: true,
+  autoMark: true
+});
+
+//Reports an error if there is a problem connecting to the database
+client.on('error', (err) => {
+    console.log('Error ' + err);
+});
+
+//Confirms the bot connected to the database
+client.on('connect', () => {
+  console.log('Connected to Redis!');
+});
+
+
+//Authenticates this app for Google Sheets
+setInterval(function () {
+  //var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE) + '/.credentials/';    //Original token directory code
+  var TOKEN_DIR = 'U:\javascript\pingbot\.credentials\\';
+  var TOKEN_PATH = TOKEN_DIR + 'sheets.googleapis.com-nodejs-quickstart.json';
+  var clientSecret = process.env.GOOGLE_CLI_SEC;
+  var clientId = process.env.GOOGLE_CLI_ID;
+  var redirectUrl = process.env.GOOGLE_REDIR_URL;
+  var auth = new googleAuth();
+  var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+  
+  fs.readFile(TOKEN_PATH, function(err, token) {
+    if (err) {
+      console.log('Error reading Token.');
+    } else {
+      oauth2Client.credentials = JSON.parse(token);
+      pullSheetsData(oauth2Client);
+	  console.log('Data was pulled successfully');
+    }
+  });
+}, 30000);	
+
+//Pulls the data from google sheets and stores it into the redis database.
+function pullSheetsData(auth) {
+  var sheets = google.sheets('v4');
+  sheets.spreadsheets.values.get({
+    auth: auth,
+    spreadsheetId: process.env.GOOGLE_SPDSHT_ID,
+    range: 'Directory!A2:M',
+  }, function(err, response) {
+    if (err) {
+      console.log('The API returned an error: ' + err);
+      return;
+    }
+    var rows = response.values;
+    if (rows.length == 0) {
+      console.log('No data found.');
+    } else {
+		//console.log(JSON.stringify(rows));
+		for (var i = 0; i < rows.length; i++) {
+			var row = rows[i];
+			
+			//initializes variables for storing
+			let key = row[0].toLowerCase();
+			client.del(key);
+			let slackid = row[3].toLowerCase();
+			let cellnum = '+' + row[4];
+			let escalation = row[8];
+			let escalationtime = row[9] * 60;
+			 
+
+			//console.log(JSON.stringify(row));
+			//makes sure there are enough arguments
+			if (escalation == null){
+				console.log('Oops! You need to enter Name, slackid, cell number, and person to be esacalated to. :(');
+				return;
+			}
+
+			//stores them in the database
+			client.rpush(key, [slackid,cellnum,escalation,escalationtime], (err) => {
+			if (err) {
+				console.log('Oops! There was an error when trying to pull the data from sheets :(');
+			}
+			});
+			  
+		}
+    }
+  });
+}
+
+//This runs every 10 seconds to clear out old message entries, clear out pings that were responded to, and escalate pings.
+setInterval(function () {
+  for(var stalkedperson in stalked_people){
+	  let stalkedUser = bot.getMemberbyID(stalkedperson);
+	  if(stalkedUser.presence == 'active'){
+		for(var i = Object.keys(stalked_people[stalkedperson]).length - 1; i >= 0; i--){
+			let dm = bot.getMemberDMbyName(stalked_people[stalkedperson][i]);
+			console.log(JSON.stringify(dm));
+			bot.slack.sendMessage(`${stalkedUser.name} is now active`, dm.id);
+		}
+		stalked_people[stalkedperson].splice(i,1);
+	  }
+  }
+
+}, 10000);
+
+//This runs every X seconds to clear out old message entries, clear out pings that were responded to, and escalate pings.
+setInterval(function () {
+	
+	removeOldMessages();
+	
+	removeMatchedPings();
+	
+	escalateMissedPing();
+	
+	console.log('Message JSON:');
+	console.log(JSON.stringify(messages_in_channels));
+	console.log('Ping JSON:');
+	console.log(JSON.stringify(pinged_in_channels));
+	console.log('Stalk JSON:');
+	console.log(JSON.stringify(stalked_people));
+}, 30000);
+
+
+//This runs every X seconds to ping Slack so the bot doesn't time out.
+setInterval(function () {
+	
+	bot.ping();
+	
+}, 5000);
+
+
+//Pings the person and stores it in the pinged JSON object
+bot.respondTo('ping', (message, channel, user) => {
+	
+  bot.setTypingIndicator(message.channel);
+  let members = bot.getMembersByChannel(channel);   //gets information of everyone in the channel and the person to ping
+  let key = getArgs(message.text).shift();
+
+  
+  client.lrange(key, 0, -1, (err, reply) => {		//pulls information from the database
+    if (err) {
+		console.log(err);
+		bot.send('Oops! There was an error pulling that information from the database. :(', channel);
+		return;
+    }
+
+	if(message.channel.toString().charAt(0) == 'D'){			//error if it is a direct message
+		bot.send('You can\'t ping in a Direct Message chat room', channel);
+		return;
+	}
+
+	/*
+	if ((members.indexOf(reply[0]) < 0)) {						//if -1 is returned, the person is not in the chat or inactive
+		bot.send(`Sorry ${user.name}, but I either can't find ${reply} in this channel, they are offline, or they are a bot!`, channel);
+		return;
+	}
+	*/
+	
+	let msgtopingee = `You are needed in ${channel.name}: slack://channel?id=${channel.id}&team=T025W1LAM`;			//define message to send, extract phone number, and get the direct message
+	let phonenum = reply[1];
+	let dm = bot.getMemberDMbyName(reply[0]);
+	
+	if(dm == undefined){										//bot gives an error in the channel if there isn't a direct message open or sends a messsage if it is opened.
+	  bot.send(`Sorry ${user.name}, I cannot send that user a direct message because he does not have a direct message with me open.`, channel);
+	}
+	else{
+	  bot.slack.sendMessage(msgtopingee, dm.id);
+	}
+
+	twilioText(phonenum, msgtopingee);
+	twilioCall(phonenum);
+    //sendemail(phonenum, msgtopingee,channel.id);			//calls sendmail function and stores the ping into the ping JSON object
+    storePingInfo(reply[0], message.ts, key, message.channel, reply[3]);
+
+  });
+}, true);
+
+
+//Stores all messages in a JSON object
+bot.respondTo('test', (message, channel, user) => {
+
+	if(!user){
+		return;
+	}
+	
+	let textMessage = 'Testing message';
+	let extras = {
+		"attachments": [
+        {
+            "text": "Choose a game to play",
+            "fallback": "You are unable to choose a game",
+            "callback_id": "wopr_game",
+            "color": "#3AA3E3",
+            "attachment_type": "default",
+            "actions": [
+                {
+                    "name": "game",
+                    "text": "Chess",
+                    "type": "button",
+                    "value": "chess"
+                },
+                {
+                    "name": "game",
+                    "text": "Falken's Maze",
+                    "type": "button",
+                    "value": "maze"
+                },
+                {
+                    "name": "game",
+                    "text": "Thermonuclear War",
+                    "style": "danger",
+                    "type": "button",
+                    "value": "war",
+                    "confirm": {
+                        "title": "Are you sure?",
+                        "text": "Wouldn't you prefer a good game of chess?",
+                        "ok_text": "Yes",
+                        "dismiss_text": "No"
+                    }
+                }
+            ]
+	}]};
+	
+	let compiledMessage = {
+		'token' : process.env.SLACKTOKEN,
+		'channel' : channel.id,
+		'text': textMessage
+	}
+	
+	console.log(JSON.stringify('Channel ID: ',channel.id));
+	console.log(JSON.stringify('Text Message :',textMessage));
+	//bot.sendChatMessage(channel.id, textMessage);
+	bot.sendChatMessage(channel.id, textMessage, extras);
+
+}, true);
+
+
+//Test for time outputs in javascript
+bot.respondTo('time', (message, channel, user) => {
+
+	let d = new Date();
+	let theDay = d.getDay();
+	let theHour = d.getHours();
+	let theMinute = d.getMinutes();
+	
+	let theTime = theDay.toString() + " " + theHour.toString() + ":" + theMinute.toString();
+	
+	console.log(theDay);
+	console.log(theHour);
+	console.log(theMinute);
+	console.log(theTime);
+	
+}, true);
+
+//Notifies you when the user becomes active again.
+bot.respondTo('stalk', (message, channel, user) => {
+	
+  bot.setTypingIndicator(message.channel);
+  let key = getArgs(message.text).shift();
+  
+  let stalkedUser = bot.getMemberbyName(key);
+  
+  if (stalked_people[stalkedUser.id]){
+	stalked_people[stalkedUser.id].push(user.name);
+  }
+  else {
+	stalked_people[stalkedUser.id] = [];
+	stalked_people[stalkedUser.id].push(user.name);
+  }
+  
+  console.log('User JSON:');
+  console.log(JSON.stringify(stalkedUser));
+  console.log('Stalk JSON:');
+  console.log(JSON.stringify(stalked_people));
+  bot.send(`Now stalking ${key}.`, channel);
+  
+}, true);
+
+//Stores all messages in a JSON object
+bot.respondTo('', (message, channel, user) => {
+	if(!user){
+		return;
+	}
+	
+    storeMessageInfo(user.name, message.ts, message.channel);
+	
+	console.log('Message JSON:');
+	console.log(JSON.stringify(messages_in_channels));
+	console.log('Ping JSON:');
+	console.log(JSON.stringify(pinged_in_channels));
+	console.log('Stalk JSON:');
+	console.log(JSON.stringify(stalked_people));
+	
+}, true);
+	
+//Stores the ping information into a JSON object
+function storePingInfo(username, thetime, id, channel, escalationtime) {
+  var holder = {user : username, timestamp : thetime, who: id, time: escalationtime};
+  
+  if (pinged_in_channels[channel]){
+	pinged_in_channels[channel].push(holder);
+  }
+  else {
+	pinged_in_channels[channel] = [];
+	pinged_in_channels[channel].push(holder);
+  }
+}
+
+//Stores the message information into a JSON object
+function storeMessageInfo(username, thetime,  channel) {
+	
+  var holder = {user : username, timestamp : thetime};
+  
+  if (messages_in_channels[channel]){
+	messages_in_channels[channel].push(holder);
+  }
+  else {
+	messages_in_channels[channel] = [];
+	messages_in_channels[channel].push(holder);
+  }
+}
+
+//Looks through the pinged list for people that haven't resopnded in the given room yet.
+//If it has been over the given amount of time, it runs a modified ping process to escalate to the next person.
+function escalateMissedPing(){
+	for(var slackchannel1 in pinged_in_channels){
+		for(var i = Object.keys(pinged_in_channels[slackchannel1]).length - 1; i >= 0; i--){
+			if(pinged_in_channels[slackchannel1][i]['timestamp'] < (Math.floor(new Date() / 1000) - pinged_in_channels[slackchannel1][i]['time'])){
+				
+				let thischannel = bot.getChannel(slackchannel1);
+				let members = bot.getMembersByChannel(thischannel);
+				
+				client.lrange(pinged_in_channels[slackchannel1][i]['who'], 0, -1, (err, reply) => {
+					if (err) {
+					 console.log(err);
+					 bot.send('Oops! I tried to retrieve something but something went wrong :(', thischannel);
+					 return;
+					}
+					
+					bot.send(`ping ${reply[2]}`, thischannel);
+					console.log('Pinging new person :');
+					console.log(JSON.stringify(pinged_in_channels[slackchannel1][i]));
+					
+					client.lrange(reply[2], 0, -1, (err, newpingee) => {
+						if (err) {
+						 console.log(err);
+						 bot.send('Oops! I tried to retrieve something but something went wrong :(', channel.id);
+						 return;
+						}
+						
+						if ((members.indexOf(newpingee[0]) < 0)) {
+							bot.send(`Sorry ${user.name}, but I either can't find ${newpingee} in this channel, or they are a bot!`, thischannel);
+							return;
+						}
+						
+						
+						let msgtopingee = `You are needed in ${thischannel.name}: slack://channel?id=${thischannel.id}&team=T025W1LAM`;
+						let phonenum = newpingee[1];
+						
+						let dm = bot.getMemberDMbyName(newpingee[0]);
+						bot.slack.sendMessage(msgtopingee, dm.id);
+						
+						sendemail(phonenum, msgtopingee,thischannel.id);			//calls sendmail function and stores the ping into the ping JSON object
+						let thetime = (Math.floor(new Date() / 1000));
+						storePingInfo(newpingee[0], thetime, reply[2], slackchannel1);
+
+					});
+					
+					pinged_in_channels[slackchannel1].splice(i,1);
+				});	
+			}
+		}
+	}
+};
+
+//Iterates over the messages that have been sent and cleared out anything that is more than x seconds old
+//Based on the last number in the "(Math.floor(new Date() / 1000) - 120)" expression
+function removeOldMessages(){
+	for(var slackchannel2 in messages_in_channels){
+		for(var i = Object.keys(messages_in_channels[slackchannel2]).length - 1; i >= 0; i--){
+			if(messages_in_channels[slackchannel2][i]['timestamp'] < (Math.floor(new Date() / 1000) - 120)){
+				console.log('Removing old message:');
+				console.log(JSON.stringify(messages_in_channels[slackchannel2][i]));
+				messages_in_channels[slackchannel2].splice(i,1);
+			}
+		}
+	}
+}
+
+//compares the list of messages and the people who have been pinged. 
+//If the person has responded in the same room, it removes them from the pinged list.
+function removeMatchedPings(){
+	for(var slackchannel1 in pinged_in_channels){
+		for(var slackchannel2 in messages_in_channels){
+			if(slackchannel1 == slackchannel2){
+				for(var slackgroup2 in messages_in_channels[slackchannel2]){
+					for(var i = Object.keys(pinged_in_channels[slackchannel1]).length - 1; i >= 0; i--){
+						if(pinged_in_channels[slackchannel1][i]['user'] == messages_in_channels[slackchannel2][slackgroup2]['user']){
+							console.log('Removing ping that were responded to :');
+							console.log(pinged_in_channels[slackchannel1][i]);
+							pinged_in_channels[slackchannel1].splice([i],1);
+						}							
+					}
+				}
+			}
+		}
+	}
+}
+
+//Take the message text and return the arguments
+function getArgs(msg) {
+  return msg.split(' ').slice(1);
+}
+
+
+// Pass in parameters to the REST API using an object literal notation. The
+// REST client will handle authentication and response serialzation for you.
+function twilioText(userPhone, textMessage){
+	twilioclient.messages.create({
+		to: userPhone,
+		from: process.env.TWILIO_PHONE_NUM,
+		body: textMessage
+	}, function(error, message) {
+		// The HTTP request to Twilio will run asynchronously. This callback
+		// function will be called when a response is received from Twilio
+		// The "error" variable will contain error information, if any.
+		// If the request was successful, this value will be "falsy"
+		if (!error) {
+			// The second argument to the callback will contain the information
+			// sent back by Twilio for the request. In this case, it is the
+			// information about the text messsage you just sent:
+			console.log('Success! The SID for this SMS message is:');
+			console.log(message.sid);
+	 
+			console.log('Message sent on:');
+			console.log(message.dateCreated);
+		} else {
+			console.log(error);
+		}
+	});
+}
+
+function twilioCall(userPhone){
+	twilioclient.calls.create({
+		url: process.env.TWILIO_PHONE_URL,
+		from: process.env.TWILIO_PHONE_NUM,
+		to:userPhone,
+	}, function(err, call) {
+		if(err){
+		  console.log(err);
+		}else{
+		  console.log('call successful');
+		}
+	});
+}
+
+
+//DEPRECATED FUNCTIONS
+/*
+//Allows users to store a call name, slack id, cell number, and escalation person in the database
+bot.respondTo('store', (message, channel, user) => {
+  let args = getArgs(message.text);
+
+  //initializes variables for storing
+  let key = args.shift();
+  client.del(key);
+  let slackid = args[0];
+  let cellnum = args[1];
+  let escalation = args[2];
+ 
+  //makes sure there are enough arguments
+  if (escalation == null){
+	  bot.send('Oops! You need to enter Name, slackid, cell number, and person to be esacalated to. :(', channel);
+	  return;
+	}
+
+  //stores them in the database
+  client.rpush(key, [slackid,cellnum,escalation], (err) => {
+    if (err) {
+      bot.send('Oops! There was an error when trying to store the data :(', channel);
+    } else {
+      bot.send(`Okay ${user.name}, I will remember that for you.`, channel);
+    }
+  });
+}, true);
+
+//Allows user to see what information is stored in a given key
+bot.respondTo('retrieve', (message, channel, user) => {
+  bot.setTypingIndicator(message.channel);
+  
+  //gets key from user
+  let key = getArgs(message.text).shift();
+  
+  //gets list from database
+  client.lrange(key, 0, -1, (err, reply) => {
+    if (err) {
+     console.log(err);
+     bot.send('Oops! There was an error when trying to retrieve that value of that key :(', channel);
+     return;
+    }
+
+    bot.send('Here\'s what I remember: ' + reply, channel);
+  });
+}, true);
+
+
+//The sendmail function taken from the sendmail/nodemailer module
+function sendemail(phonenum, emailmsg, slackroomid){
+
+	let emailrecipient = phonenum + '@vzwpix.com';
+	
+	let transporter = nodemailer.createTransport({
+		service: 'Gmail',
+		auth: {
+			user: 'XXXXXXXX',
+			pass:  'XXXXXXXXXXXX'
+		},
+		debug: true // include SMTP traffic in the logs
+	}, {
+		// sender info
+		from: 'XXXXXXXXXXXXX',
+	});
+
+	let message = {
+		to: emailrecipient,
+		subject: 'You\'re needed in Slack', 
+		text: emailmsg,
+		html: emailmsg,
+	};
+
+	transporter.sendMail(message, (error, info) => {
+		if (error) {
+			console.log('Error occurred with the transporter.');
+			console.log(error.message);
+			return;
+		}
+		console.log('Message sent successfully!');
+		console.log(JSON.stringify(info));
+		console.log('Server responded with "%s"', info.response);
+		transporter.close();
+	});
+}	
+*/
